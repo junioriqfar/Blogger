09f319838de3864e3cf57967cb11a1412008ce32
junioriqfar <junioriqfar@gmail.com>
2017-06-17 15:28:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function(){
+jQuery("#likebox_1").hover(function(){ jQuery(this).stop(true,false).animate({right:  0}, 500); },function(){ jQuery("#likebox_1").stop(true,false).animate({right: -200}, 500); });
+jQuery("#polecam_1").hover(function(){ jQuery(this).stop(true,false).animate({right:  0}, 500); },function(){ jQuery("#polecam_1").stop(true,false).animate({right: -250}, 500); }); 
+jQuery("#google_1").hover(function(){ jQuery(this).stop(true,false).animate({right:  0}, 500); },function(){ jQuery("#google_1").stop(true,false).animate({right: -154}, 500); }); 
+jQuery("#youtube_1").hover(function(){ jQuery(this).stop(true,false).animate({right:  0}, 500); },function(){ jQuery("#youtube_1").stop(true,false).animate({right: -303}, 500); });});
